e7df3a5e88b02d6c1c0ee268315b968bbacdd7a3
parkson-tano <tanoparksonsilencer@gmail.com>
2022-12-24 10:29:11 +0100

    add i

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -28,7 +28,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'password', 'phone_number', 'date_created', )
+        fields = ('id', 'password', 'phone_number','momo_account', 'date_created', )
 
 
 class RegisterSerializer(serializers.ModelSerializer):
