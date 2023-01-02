38c90a60a8b2419563557c88a82b169064e748d0
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-01-03 00:24:29 +0100

    Update views.py

--- backend/balance/views.py
+++ backend/balance/views.py
@@ -86,11 +86,8 @@ def history(request, number):
 
 @api_view(['GET', 'PATCH'])
 def search(request, number):
-    mtn_account = User.objects.get(phone_number = number)
-
-    if mtn_account:
-        user_serializer = GetUserSerializer(
+    mtn_account = User.objects.filter(phone_number = number)
+    user_serializer = GetUserSerializer(
             mtn_account, context={'request': request})
-        return Response(user_serializer.data)
-    else:
-        pass
+    return Response(user_serializer.data)
+
